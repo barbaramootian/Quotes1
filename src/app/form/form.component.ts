2869746e72a111bc0336e251c4a654175613df06
barbaramootian <barbara.mootian@student.moringaschool.com>
2022-01-06 21:46:03 +0300

    Added form componet

--- src/app/form/form.component.ts
+++ src/app/form/form.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Output, EventEmitter } from '@angular/core';
+import { Quote } from '../quote';
 
 @Component({
   selector: 'app-form',
@@ -6,10 +7,15 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./form.component.css']
 })
 export class FormComponent implements OnInit {
-
+@Output() formemit:any = new EventEmitter()
   constructor() { }
-
+   
   ngOnInit(): void {
   }
-
+myQuote : Quote = new Quote("", "", "", new Date())
+receive(formDetails:any){
+  let myFormDetails = new Quote(formDetails.value.name, formDetails.value.quote, formDetails.value.author, formDetails.value.date)
+  this.formemit.emit(myFormDetails)
+  formDetails.reset()
+}
 }
