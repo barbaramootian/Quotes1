878f0e027db6dcb070b943cc1231c6f291997cb5
barbaramootian <barbara.mootian@student.moringaschool.com>
2022-01-07 13:47:44 +0300

    add quote details ts

--- src/app/quote-details/quote-details.component.ts
+++ src/app/quote-details/quote-details.component.ts
@@ -1,4 +1,3 @@
-
 import { Component, OnInit } from '@angular/core';
 import { Quote } from '../quote';
 
@@ -8,13 +7,28 @@ import { Quote } from '../quote';
   styleUrls: ['./quote-details.component.css']
 })
 export class QuoteDetailsComponent implements OnInit {
+  totalCount: number = 0;
 
   constructor() { }
 
   ngOnInit(): void {
   }
-myQuote: Quote[] = [new Quote("Barbra", "Self Love ambassador", "Reson", new Date(2021,12,31))]
-emmitedDetails(details:any){details.date=new Date(details.date)
-  this.myQuote.push(details)
-}
+
+  myQuote: Quote[] = [new Quote("Barbra", "Self Love ambassador", "Reson", new Date(2021,12,31))]
+  emmitedDetails(details:any){details.date=new Date(details.date)
+    this.myQuote.push(details)
+  }
+  upvote(i:number){
+    this.myQuote[i].upvotenumber = this.myQuote[i].upvotenumber+1
+    if (this.myQuote[i].upvotenumber>this.totalCount )(this.totalCount = this.myQuote[i].upvotenumber)
+  }
+  myCompletedquote(completedquote:boolean, index:number){
+    if (completedquote) {
+      let toDelete = confirm(`Are you sure you want to delete ${this.myQuote[index].name}?`)
+
+      if (toDelete){
+        this.myQuote.splice(index,1)
+      }
+    }
+  }
 }
--- src/app/quotes/quotes.component.ts
+++ src/app/quotes/quotes.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
+import { Quote } from '../quote';
 
 @Component({
   selector: 'app-quotes',
@@ -6,10 +7,18 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./quotes.component.css']
 })
 export class QuotesComponent implements OnInit {
-
+  totalCount: number=0;
+  @Input() quote : any
   constructor() { }
 
   ngOnInit(): void {
   }
-
+  myQuote: Quote[] = [new Quote("Barbra", "Self Love ambassador", "Reson", new Date(2021,12,31))]
+  emmitedDetails(details:any){details.date=new Date(details.date)
+    this.myQuote.push(details)
+  }
+  upvote(i:number){
+    this.myQuote[i].upvotenumber = this.myQuote[i].upvotenumber+1
+    if (this.myQuote[i].upvotenumber>this.totalCount )(this.totalCount = this.myQuote[i].upvotenumber)
+  }
 }
